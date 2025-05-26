f867d13afa2a898aed8d0201841aebce551ce145
malvinarum <kaanyenidag@windowslive.com>
2025-05-26 15:03:22 +0300

    Initial commit

--- systemmonitor/systemmonitor.py
+++ systemmonitor/systemmonitor.py
@@ -40,7 +40,8 @@ class SystemMonitor(commands.Cog):
         embed.add_field(name="Disk", value=f"{disk_usage}%", inline=True)
         embed.add_field(name="Upload Speed", value=f"{network_sent_speed:.2f} MB/sec", inline=True)
         embed.add_field(name="Download Speed", value=f"{network_received_speed:.2f} MB/sec", inline=True)
-        embed.set_footer(text=f"Last Updated: <t:{int(datetime.now().timestamp())}:R>")
+        embed.set_footer(
+            text=f"Last Updated: {(datetime.now().astimezone(timezone('US/Eastern'))).strftime('%Y-%m-%d %H:%M:%S')} EST")
 
         if self.message:
             await self.message.edit(embed=embed)
