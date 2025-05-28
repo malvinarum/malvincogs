edae5cabe6851d2e236a33b50171cb303ace7282
malvinarum <kaanyenidag@windowslive.com>
2025-05-28 23:01:29 +0300

    Added plextracker

--- systemmonitor/__init__.py
+++ systemmonitor/__init__.py
@@ -11,3 +11,4 @@ class SystemMonitor(commands.Cog):
 from .systemmonitor import SystemMonitor
 async def setup(bot):
     await bot.add_cog(SystemMonitor(bot))
+    await bot.add_cog(PlexActivity(bot))
