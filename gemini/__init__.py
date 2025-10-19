e14fb5a44850d8ae4c565f1078b722e986437378
malvinarum <kaanyenidag@windowslive.com>
2025-10-19 20:30:35 +0300

    .

--- gemini/__init__.py
+++
@@ -1,9 +0,0 @@
-from .gemini import Gemini # Changed 'gemini' to 'Gemini' (capital G)
-
-async def setup(bot):
-    """
-    This function is called by Redbot when the cog is loaded.
-    It adds the Gemini cog to the bot.
-    """
-    await bot.add_cog(Gemini(bot))
-
